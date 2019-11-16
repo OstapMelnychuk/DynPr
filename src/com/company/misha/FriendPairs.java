a2f881696f52872799b05bf1784e9b7b13a43fbd
Misha <miwka0806@gmail.com>
2019-11-16 15:54:18 +0200

    done but without JavaDoc

--- src/com/company/misha/FriendPairs.java
+++ src/com/company/misha/FriendPairs.java
@@ -1,4 +1,30 @@
 package com.company.misha;
 
 public class FriendPairs {
+    private int numberOfFriends;
+
+    public FriendPairs(int numberOfFriends) {
+        this.numberOfFriends = numberOfFriends;
+    }
+
+    public void findNumberOfPair() {
+        try {
+            int mas[] = new int[numberOfFriends + 1];
+
+            for (int i = 0; i <= numberOfFriends; i++) {
+                if (i <= 2) {
+                    mas[i] = i;
+                } else {
+                    mas[i] = mas[i - 1] + (i - 1) * mas[(i - 2)];
+                }
+            }
+
+            System.out.println(mas[numberOfFriends]
+                    + " is number of ways in which friends can remain single or can be paired up.");
+        }catch (NegativeArraySizeException e){
+            System.out.println("The number cannot be negative");
+        }
+    }
+
+
 }
--- src/com/company/misha/LongestSubsequence.java
+++ src/com/company/misha/LongestSubsequence.java
@@ -1,4 +1,57 @@
 package com.company.misha;
 
 public class LongestSubsequence {
+    private String sequence;
+    private String sequenceOfNumbers[];
+    private int masOfNumbersInTheSequence[];
+
+    public LongestSubsequence(String sequence) {
+        this.sequence = sequence;
+        sequenceOfNumbers = sequence.split(" ");
+        masOfNumbersInTheSequence = new int[sequenceOfNumbers.length];
+    }
+
+    public void findTheLongestSubsequence() {
+        int theLongestSubsequences = 0;
+
+        boolean isCorrectlyEntered = countSubsequences();
+
+        if (isCorrectlyEntered) {
+            for (int i = 0; i < masOfNumbersInTheSequence.length; i++) {
+                if (theLongestSubsequences < masOfNumbersInTheSequence[i]) {
+                    theLongestSubsequences = masOfNumbersInTheSequence[i];
+                }
+            }
+
+            System.out.println("The longest subsequence is " + theLongestSubsequences);
+        }
+    }
+
+    private boolean countSubsequences() {
+        int count = 1;
+        int numberOfSubSequence = 0;
+
+        for (int i = 1; i < sequenceOfNumbers.length; i++) {
+            try {
+                if ((Integer.parseInt(sequenceOfNumbers[i]) - Integer.parseInt(sequenceOfNumbers[i - 1]) == 1)
+                        || (Integer.parseInt(sequenceOfNumbers[i - 1]) - Integer.parseInt(sequenceOfNumbers[i]) == 1)) {
+                    count++;
+                } else {
+                    masOfNumbersInTheSequence[numberOfSubSequence] = count;
+                    count = 1;
+                    numberOfSubSequence++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("The numbers are incorrectly entered. \"" +
+                        sequenceOfNumbers[i] + "\" is not a number");
+
+                return false;
+            }
+        }
+
+        masOfNumbersInTheSequence[numberOfSubSequence] = count;
+
+        return true;
+    }
+
 }
