5681ae58a7b14616f7598760694ac8f264ba9b63
Oleksiy Radyuk <mi.alexhack14@gmail.com>
2019-11-18 21:26:22 +0200

    Solve task three and add documentation to task ten

--- src/com/company/alex/TaskTen.java
+++ src/com/company/alex/TaskTen.java
@@ -1,12 +1,30 @@
 package com.company.alex;
 
-// Java program to find palindromic substrings of a string
+/**
+ *
+ * This class use to find the biggest palindrome in our string.
+ *
+ * @author      Oleksiy Radyuk
+ * @version     1.0
+ * @since       2019-11-18
+ */
 public class TaskTen {
-
+  /**
+   * Find bigger element.
+   *
+   * @param x first element to compare.
+   * @param y second element to compare.
+   * @return bigger element.
+   */
   static int max(int x, int y) {
     return (x > y) ? x : y;
   }
-
+  /**
+   * Given a string which we have to check how many symbols has the biggest palindrome which is included in the given one.
+   *
+   * @param seq string which we have to check.
+   * @return the biggest palindrome.
+   */
   static int lps(String seq) {
     int n = seq.length();
     int i;
