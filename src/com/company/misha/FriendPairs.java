7cbaf1b7377ae8b9bf762b972ddabcfd5e7dd337
Misha <miwka0806@gmail.com>
2019-11-18 11:30:15 +0200

    add CheckStyle and JavaDoc

--- src/com/company/misha/FriendPairs.java
+++ src/com/company/misha/FriendPairs.java
@@ -1,25 +1,34 @@
 package com.company.misha;
 
+/**
+ * This class finds out the total number of ways in which friends can remain single
+ * or can be paired up.
+ */
 public class FriendPairs {
 
-    public void findNumberOfPair(int numberOfFriends) {
-        try {
-            int mas[] = new int[numberOfFriends + 1];
+  /**
+   * This method looks for the number of all possible ways to create pairs of numbers.
+   *
+   * @param numberOfFriends the size of a string of numbers
+   */
+  public void findNumberOfPair(int numberOfFriends) {
+    try {
+      int[] mas = new int[numberOfFriends + 1];
 
-            for (int i = 0; i <= numberOfFriends; i++) {
-                if (i <= 2) {
-                    mas[i] = i;
-                } else {
-                    mas[i] = mas[i - 1] + (i - 1) * mas[(i - 2)];
-                }
-            }
-
-            System.out.println(mas[numberOfFriends]
-                    + " is number of ways in which friends can remain single or can be paired up.");
-        }catch (NegativeArraySizeException e){
-            System.out.println("The number cannot be negative");
+      for (int i = 0; i <= numberOfFriends; i++) {
+        if (i <= 2) {
+          mas[i] = i;
+        } else {
+          mas[i] = mas[i - 1] + (i - 1) * mas[(i - 2)];
         }
+      }
+
+      System.out.println(mas[numberOfFriends]
+              + " is number of ways in which friends can remain single or can be paired up.");
+    } catch (NegativeArraySizeException e) {
+      System.out.println("The number cannot be negative");
     }
+  }
 
 
 }
--- src/com/company/misha/LongestSubsequence.java
+++ src/com/company/misha/LongestSubsequence.java
@@ -1,54 +1,84 @@
 package com.company.misha;
 
+/**
+ * This class to find how many numbers we have with difference one in the maximum sub sequence.
+ */
 public class LongestSubsequence {
-    private String sequence;
-    private String sequenceOfNumbers[];
-    private int masOfNumbersInTheSequence[];
+  /**
+   * Incoming sentence with sequences.
+   */
+  private String sequence;
+  /**
+   * An array of characters taken from sentences.
+   */
+  private String[] sequenceOfNumbers;
+  /**
+   * An array of numbers taken from sentences.
+   */
+  private int[] masOfNumbersInTheSequence;
 
-    public void findTheLongestSubsequence(String sequence) {
-        int theLongestSubsequences = 0;
+  /**
+   * This method finds how many numbers we have with different one in the maximum subsequence
+   * and finds the longest.
+   *
+   * @param sequence given subsequence
+   */
+  public void findTheLongestSubsequence(String sequence) {
+    int theLongestSubsequences = 0;
 
-        sequenceOfNumbers = sequence.split(" ");
-        masOfNumbersInTheSequence = new int[sequenceOfNumbers.length];
+    sequenceOfNumbers = sequence.split(" ");
+    masOfNumbersInTheSequence = new int[sequenceOfNumbers.length];
 
-        boolean isCorrectlyEntered = countSubsequences();
+    boolean isCorrectlyEntered = countSubsequences();
 
-        if (isCorrectlyEntered) {
-            for (int i = 0; i < masOfNumbersInTheSequence.length; i++) {
-                if (theLongestSubsequences < masOfNumbersInTheSequence[i]) {
-                    theLongestSubsequences = masOfNumbersInTheSequence[i];
-                }
-            }
-
-            System.out.println("The longest subsequence is " + theLongestSubsequences);
+    if (isCorrectlyEntered) {
+      for (int i = 0; i < masOfNumbersInTheSequence.length; i++) {
+        if (theLongestSubsequences < masOfNumbersInTheSequence[i]) {
+          theLongestSubsequences = masOfNumbersInTheSequence[i];
         }
+      }
+
+      System.out.println("The longest subsequence is " + theLongestSubsequences);
+    }
+  }
+
+  /**
+   * This method finds how many numbers we have with different one in all subsequence.
+   *
+   * @return false if we have a trouble and true if we found all numbers.
+   */
+  private boolean countSubsequences() {
+    int count = 1;
+    int numberOfSubSequence = 0;
+
+    for (int i = 1; i < sequenceOfNumbers.length; i++) {
+      try {
+        if ((Integer.parseInt(sequenceOfNumbers[i]) - Integer.parseInt(sequenceOfNumbers[i - 1]) == 1)
+                || (Integer.parseInt(sequenceOfNumbers[i - 1]) - Integer.parseInt(sequenceOfNumbers[i]) == 1)) {
+          count++;
+        } else {
+          masOfNumbersInTheSequence[numberOfSubSequence] = count;
+          count = 1;
+          numberOfSubSequence++;
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("The numbers are incorrectly entered. \""
+                + sequenceOfNumbers[i] + "\" is not a number");
+
+        return false;
+      }
     }
 
-    private boolean countSubsequences() {
-        int count = 1;
-        int numberOfSubSequence = 0;
+    masOfNumbersInTheSequence[numberOfSubSequence] = count;
 
-        for (int i = 1; i < sequenceOfNumbers.length; i++) {
-            try {
-                if ((Integer.parseInt(sequenceOfNumbers[i]) - Integer.parseInt(sequenceOfNumbers[i - 1]) == 1)
-                        || (Integer.parseInt(sequenceOfNumbers[i - 1]) - Integer.parseInt(sequenceOfNumbers[i]) == 1)) {
-                    count++;
-                } else {
-                    masOfNumbersInTheSequence[numberOfSubSequence] = count;
-                    count = 1;
-                    numberOfSubSequence++;
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("The numbers are incorrectly entered. \"" +
-                        sequenceOfNumbers[i] + "\" is not a number");
+    return true;
+  }
 
-                return false;
-            }
-        }
+  public String getSequence() {
+    return sequence;
+  }
 
-        masOfNumbersInTheSequence[numberOfSubSequence] = count;
-
-        return true;
-    }
-
+  public void setSequence(String sequence) {
+    this.sequence = sequence;
+  }
 }
