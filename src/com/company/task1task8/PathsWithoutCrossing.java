b17258195c397635d7db86d4e58c5b5204f25f0b
Ostap Melnychuk <ostapss222@gmail.com>
2019-11-19 18:34:45 +0200

    resolve

--- src/com/company/task1task8/PathsWithoutCrossing.java
+++ src/com/company/task1task8/PathsWithoutCrossing.java
@@ -30,7 +30,6 @@ public class PathsWithoutCrossing {
 
   public static int countWays(int n) {
     if (n < 1) {
-      System.out.println("Invalid");
       return 0;
     }
     if (n > Integer.MAX_VALUE / 10000) {
@@ -48,9 +47,8 @@ public class PathsWithoutCrossing {
    */
   private static int catalanNumber(int n) {
     int[] catalan = new int[n + 1];
-
-    catalan[0] = catalan[1] = 1;
-
+    catalan[0] = 1;
+    catalan[1] = 1;
     for (int i = 2; i <= n; i++) {
       catalan[i] = 0;
       for (int j = 0; j < i; j++) {
