63e4fdba44c691b30883cc39cab94dc688a5bfa2
Vasyl_Petrashchuk <rockeman2013@gmail.com>
2019-11-20 14:14:21 +0200

    Added directory test

---
+++ tests/com/company/alex/TaskTenTest.java
@@ -0,0 +1,16 @@
+package com.company.alex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaskTenTest {
+
+  @Test
+  public void max() {
+  }
+
+  @Test
+  public void lps() {
+  }
+}
---
+++ tests/com/company/alex/TaskThreeTest.java
@@ -0,0 +1,13 @@
+package com.company.alex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaskThreeTest {
+
+  @Test
+  public void interestingRow() {
+  }
+
+}
