c536b69ba6ff5fad42fe1c0b8e448dc947a1fae0
Olha Yaremchuk <olga7325@gmail.com>
2019-11-20 14:27:15 +0200

    Checked with checkstyle

--- src/com/company/Olha/TaskSix.java
+++ src/com/company/Olha/TaskSix.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.Olha;
 
 import java.util.Arrays;
 
@@ -10,30 +10,31 @@ import java.util.Arrays;
  * @since 2019-11-19
  */
 public class TaskSix {
-    /**
-     * This method counts number of ways to write n as sum of two or more positive integers
-     *
-     * @param n positive integer number
-     * @return number of ways to write n as sum of two or more positive integers
-     */
+  /**
+   * This method counts number of ways to write n as sum of two or more positive integers
+   *
+   * @param n positive integer number
+   * @return number of ways to write n as sum of two or more positive integers
+   */
 
-    static int countSumWays(int n) {
-        if (n <= 0) {
-            System.out.println("Number should be positive integer (greater than 0).");
-            return 0;
-        } else {
-            int table[] = new int[n + 1];
+  static int countSumWays(int n) {
+    if (n <= 0) {
+      System.out.println("Number should be positive integer (greater than 0).");
+      return 0;
+    } else {
+      int table[] = new int[n + 1];
 
-            Arrays.fill(table, 0);
+      Arrays.fill(table, 0);
 
-            table[0] = 1;
+      table[0] = 1;
 
-            for (int i = 1; i < n; i++) {
-                for (int j = i; j <= n; j++) {
-                    table[j] += table[j - i];
-                }
-            }
-            return table[n];
+      for (int i = 1; i < n; i++) {
+        for (int j = i; j <= n; j++) {
+          table[j] += table[j - i];
         }
+      }
+      return table[n];
     }
+  }
+
 }
--- src/com/company/Olha/TaskThirteen.java
+++ src/com/company/Olha/TaskThirteen.java
@@ -1,5 +1,5 @@
 
-package com.company;
+package com.company.Olha;
 
 /**
  * This class is for Painting Fence Algorithm
