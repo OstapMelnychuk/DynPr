812f5cb7801b33716ce13fd1d07754d898c779fb
Oleksiy Radyuk <mi.alexhack14@gmail.com>
2019-11-20 14:02:34 +0200

    Add sorting

---
+++ src/com/company/sort/InsertionSort.java
@@ -0,0 +1,37 @@
+package pack;
+/**
+ *
+ * This class use to sort integer array by insertion sort.
+ *
+ * @author      Oleksiy Radyuk
+ * @version     1.0
+ * @since       2019-11-18
+ */
+
+public class InsertionSort {
+
+  /**
+   * Sorting array.
+   * @param arr which need to be sorted.
+   * @return sorted array.
+   */
+
+  public static int[] sort(int arr[]) {
+    if (arr.length > 0) {
+      int n = arr.length;
+      for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+          arr[j + 1] = arr[j];
+          j = j - 1;
+        }
+        arr[j + 1] = key;
+      }
+      return arr;
+    }
+    else {
+      return arr;
+    }
+  }
+}
