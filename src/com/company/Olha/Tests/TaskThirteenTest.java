4dd016559fa8d86df4ca3f591b3aadcdc1808b2f
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-11-20 19:50:02 +0200

    tests

---
+++ src/com/company/Olha/Tests/TaskThirteenTest.java
@@ -0,0 +1,17 @@
+package com.company.Olha.Tests;
+import com.company.Olha.TaskThirteen;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TaskThirteenTest {
+  @Test
+  public void countWays() {
+    assertEquals(16, TaskThirteen.countWays(2,4));
+    assertEquals(0,TaskThirteen.countWays(-2,4));
+    assertEquals(0,TaskThirteen.countWays(2,-4));
+    assertEquals(0,TaskThirteen.countWays(0,4));
+    assertEquals(0,TaskThirteen.countWays(4,0));
+    assertEquals(0,TaskThirteen.countWays(0,0));
+  }
+
+}
