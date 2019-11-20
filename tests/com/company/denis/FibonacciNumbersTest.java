98ce5047b3611f3bb28152ebdef85273a8e5ae9c
Denis <nechyporukdenis98@gmail.com>
2019-11-20 15:06:04 +0200

    add counting sort

---
+++ tests/com/company/denis/FibonacciNumbersTest.java
@@ -0,0 +1,19 @@
+package com.company.denis;
+
+import com.company.task1task8.FibonacciNumbers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciNumbersTest {
+
+  @Test
+  public void fibNumb() {
+    Assert.assertEquals(2, FibonacciNumbers.fibNumb(3));
+    Assert.assertEquals(1, FibonacciNumbers.fibNumb(1));
+    Assert.assertEquals(233, FibonacciNumbers.fibNumb(13));
+    Assert.assertEquals(0, FibonacciNumbers.fibNumb(-3));
+    Assert.assertEquals(0, FibonacciNumbers.fibNumb(0));
+  }
+}
