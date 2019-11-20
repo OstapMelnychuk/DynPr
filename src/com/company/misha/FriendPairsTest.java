299375ec595cda1504f31a71cfa037b4798015be
Oleksiy Radyuk <mi.alexhack14@gmail.com>
2019-11-20 10:05:51 +0200

    add test to task4 and task 10

---
+++ src/com/company/misha/FriendPairsTest.java
@@ -0,0 +1,22 @@
+package com.company.misha;
+
+import com.company.misha.FriendPairs;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FriendPairsTest {
+
+  private FriendPairs friendPairs;
+
+  @Test
+  public void findNumberOfPair() {
+    friendPairs=new FriendPairs();
+    assertEquals(4,friendPairs.findNumberOfPair(3));
+//    assertEquals(-1,friendPairs.findNumberOfPair(-1));
+    assertEquals(-1,friendPairs.findNumberOfPair(-3));
+    assertEquals(0,friendPairs.findNumberOfPair(0));
+    assertEquals(1,friendPairs.findNumberOfPair(1));
+  }
+
+}
---
+++ src/com/company/misha/LongestSubsequenceTest.java
@@ -0,0 +1,17 @@
+package com.company.misha;
+
+import com.company.misha.LongestSubsequence;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LongestSubsequenceTest {
+  LongestSubsequence lS;
+  @Test
+  public void findTheLongestSubsequence() {
+    lS=new LongestSubsequence();
+    assertEquals(-1, lS.findTheLongestSubsequence("a b d"));
+    assertEquals(3, lS.findTheLongestSubsequence("3 4 7 8 9"));
+    assertEquals(-1, lS.findTheLongestSubsequence("a 4 7 8 9"));
+  }
+}
