efdb7d6b4d83200cac4b2412050115c95ad33cf5
Misha <miwka0806@gmail.com>
2019-11-20 20:00:44 +0200

    solve some problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import com.company.task1task8.FibonacciNumbers;
-import com.company.task1task8.PathsWithoutCrossing;
-
 public class Main {
   public static void main(String[] args) {
 
