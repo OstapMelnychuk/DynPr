81c4341f440bb5f6c8c2afe4dbd626d25237c7b7
Olha Yaremchuk <olga7325@gmail.com>
2019-11-20 13:09:50 +0200

    Olha's tasks

---
+++ src/com/company/Olha/TaskSix.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Arrays;
+
+/**
+ * This class is for searching ways to write n as sum of two or more positive integers
+ *
+ * @author Olha
+ * @version 1.0
+ * @since 2019-11-19
+ */
+public class TaskSix {
+    /**
+     * This method counts number of ways to write n as sum of two or more positive integers
+     *
+     * @param n positive integer number
+     * @return number of ways to write n as sum of two or more positive integers
+     */
+
+    static int countSumWays(int n) {
+        if (n <= 0) {
+            System.out.println("Number should be positive integer (greater than 0).");
+            return 0;
+        } else {
+            int table[] = new int[n + 1];
+
+            Arrays.fill(table, 0);
+
+            table[0] = 1;
+
+            for (int i = 1; i < n; i++) {
+                for (int j = i; j <= n; j++) {
+                    table[j] += table[j - i];
+                }
+            }
+            return table[n];
+        }
+    }
+}
