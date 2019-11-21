84ccd17b5d01151c7a60ef61c0b9ee191402e6a5
Misha <miwka0806@gmail.com>
2019-11-21 15:03:33 +0200

    add all sort in sort-menu

--- src/com/company/sort/BucketSort.java
+++ src/com/company/sort/BucketSort.java
@@ -1,7 +1,5 @@
 package com.company.sort;
 
-import java.util.Arrays;
-
 /**
  * This class is for Bucket Sort.
  *
@@ -10,33 +8,67 @@ import java.util.Arrays;
  * @since 2019-11-20
  */
 
-public class BucketSort {
+public class BucketSort implements Sort {
 
   /**
    * This method realises Bucket Sort.
    *
-   * @param array  this is an array which will besorted
-   * @param maxVal maximum value (integer number)
+   * @param array this is an array which will besorted
    */
-
-  static void bucketSort(int[] array, int maxVal) {
-    int[] bucket = new int[maxVal + 1];
-
-    for (int i = 0; i < bucket.length; i++) {
-      bucket[i] = 0;
-    }
-
-    for (int i = 0; i < bucket.length; i++) {
-      bucket[array[i]]++;
-    }
-
-    int outPos = 0;
-    for (int i = 0; i < bucket.length; i++) {
-      for (int j = 0; j < bucket[i]; j++) {
-        array[outPos++] = i;
+  @Override
+  public int[] sort(int[] array) {
+    boolean negative = false;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < 0) {
+        negative = true;
       }
-    }
 
+    }
+    if (negative) {
+      System.out.println("Negative, we can't applie this typeof sorting for array of negative nubers.");
+      return array;
+    } else {
+      int maxVal = 0;
+      int minVal = 0;
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] > maxVal) {
+          maxVal = array[i];
+        } else {
+          if (array[i] < minVal) {
+            minVal = array[i];
+          }
+        }
+
+      }
+      System.out.println(maxVal);
+      System.out.println(minVal);
+      if (Math.abs(minVal) > maxVal) {
+        maxVal += Math.abs(minVal) * 2;
+        System.out.println(maxVal);
+      } else {
+        maxVal = maxVal * 2;
+        System.out.println(maxVal);
+      }
+
+      int[] bucket = new int[maxVal + 1];
+
+      for (int i = 0; i < bucket.length; i++) {
+        bucket[i] = 0;
+      }
+
+      for (int i = 0; i < array.length; i++) {
+        bucket[array[i]]++;
+      }
+
+      int outPos = 0;
+      for (int i = 0; i < bucket.length; i++) {
+        for (int j = 0; j < bucket[i]; j++) {
+          array[outPos++] = i;
+        }
+      }
+
+    }
+    return array;
   }
 
 }
