46359df841b9c12f82379dec1c8cd15ae812dc7a
Vasyl_Petrashchuk <rockeman2013@gmail.com>
2019-11-21 16:57:19 +0200

    Final

--- tests/com/company/alex/TaskTenTest.java
+++ tests/com/company/alex/TaskTenTest.java
@@ -1,20 +1,36 @@
 package com.company.alex;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TaskTenTest {
+  TaskTen t1;
+
+  @Before
+  public void setUpTaskTen() {
+    t1 = new TaskTen();
+
+  }
+
+  @After
+  public void tearDownTenTest() {
+    t1 = null;
+  }
 
   @Test
   public void max() {
-    for(int i=-15;i<15;i+=4){
-    assertEquals(i, TaskTen.max(i,i-5));
-  }
+    assertEquals(5, t1.max(4, 5));
+    assertEquals(5, t1.max(5, 4));
+    assertEquals(0, t1.max(-1, 0));
+    assertEquals(1, t1.max(1, -1));
+
   }
 
   @Test
   public void lps() {
-
+    assertEquals(5, t1.lps("banana"));
   }
 }
--- tests/com/company/alex/TaskThreeTest.java
+++ tests/com/company/alex/TaskThreeTest.java
@@ -1,14 +1,27 @@
 package com.company.alex;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
 public class TaskThreeTest {
+  TaskThree t3;
+
+  @Before
+  public void setUpTaskThree() {
+    t3 = new TaskThree();
+
+  }
+
+  @After
+  public void tearDownThreeTest() {
+    t3 = null;
+  }
 
   @Test
   public void interestingRow() {
-    assertEquals(0,0);
+    assertEquals(1, t3.interestingRow(1));
+    assertEquals(11, t3.interestingRow(5));
   }
 
 }
