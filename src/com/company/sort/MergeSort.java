8397f9b279bd19a0f22ef8ae2414e9aee6ac8a0f
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-11-21 11:44:50 +0200

    add_java.doc

--- src/com/company/sort/MergeSort.java
+++ src/com/company/sort/MergeSort.java
@@ -1,10 +1,25 @@
 package com.company.sort;
 
-import java.util.Arrays;
+/**
+ * Marge sort for array of integers.
+ *
+ * @author Vasyl Petrashchuk
+ * @version 1.3 18 Nov 2019
+ */
 
-public class MergeSort {
+public class MergeSort implements Sort {
+  /**
+   * This is input array of integers to be sorted.
+   */
   private static int[] arrToSort;
 
+
+  /**
+   * This method create workspace array and invoke method reqMergeSort.
+   *
+   * @param arr which need to be sorted.
+   * @return sorted array.
+   */
   public int[] sort(int[] arr) {
     arrToSort = arr;
     int[] workSpace = new int[arr.length];
@@ -12,6 +27,12 @@ public class MergeSort {
     return arrToSort;
   }
 
+  /**
+   * This method divide array on sub-arrays and invoke merge method.
+   * @param arr      which need to be sorted.
+   * @param lowBound lowerBound of array to sort
+   * @param upBound  upperBound of array to sort
+   */
   private static void reqMergeSort(int[] arr, int lowBound, int upBound) {
     if (lowBound == upBound) {
       return;
@@ -22,6 +43,13 @@ public class MergeSort {
     merge(arr, lowBound, mid + 1, upBound);
   }
 
+  /**
+   * This method sort array.
+   * @param workSpace    which need to be sorted.
+   * @param lowPtr lowerBound of array to sort
+   * @param highPtr  middle
+   * @param upperBound upperBound of array to sort
+   */
   private static void merge(int[] workSpace, int lowPtr, int highPtr, int upperBound) {
     int j = 0;
     int lowBound = lowPtr;
